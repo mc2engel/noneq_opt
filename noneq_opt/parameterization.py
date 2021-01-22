6da023cfb967c22c437ee1ce172405a7232ce02f
Jamie Smith <jamieas@google.com>
2021-01-22 15:06:35 -0500

    Add piecewise linear and Chebyshev parameterizations.

--- noneq_opt/parameterization.py
+++ noneq_opt/parameterization.py
@@ -5,9 +5,61 @@ import typing
 import jax
 import jax.numpy as jnp
 import numpy as np
+import scipy.special as sps
+
 
 class Constant(typing.NamedTuple):
     value: jnp.array
 
     def __call__(self, x):
         return self.value * jnp.ones_like(self.value)
+
+
+class PiecewiseLinear(typing.NamedTuple):
+    values: jnp.array
+
+    @property
+    def length(self):
+        return self.values.shape[-1]
+
+    @property
+    def x(self):
+        return jnp.linspace(0, 1, self.length)
+
+    def __call__(self, x):
+        return jnp.interp(
+        x, self.x, self.values, left=self.values[0], right=self.values[-1])
+
+
+def chebyshev_coefficients(degree):
+    # TODO: consider other Chebyshev polynomial type.
+    return np.stack([
+        np.concatenate([np.zeros(degree - j), sps.chebyt(j, True)])
+        for j in range(degree + 1)])
+
+
+class Chebyshev(typing.NamedTuple):
+  weights: jnp.array
+
+  @property
+  def degree(self):
+    return self.weights.shape[0] - 1
+
+  @property
+  def coefficients(self):
+    return chebyshev_coefficients(self.degree)
+
+  def _powers(self, x):
+    def _multiply_by_x(y, _):
+      y *= x
+      return y, y
+    ones = jnp.ones_like(x)
+    _, powers = jax.lax.scan(
+        _multiply_by_x, ones, None, length=self.degree, reverse=True)
+    return jnp.concatenate([powers, ones[jnp.newaxis]], axis=0)
+
+  def __call__(self, x):
+    x = 2. * x - 1.  # Rescale (0, 1) -> (-1, 1)
+    x_powers = self._powers(x)
+    return jnp.einsum(
+        'w,wp,p...->...', self.weights, self.coefficients, x_powers)
--- tests/parameterization_test.py
+++ tests/parameterization_test.py
@@ -8,16 +8,27 @@ import numpy as np
 
 import noneq_opt.parameterization as p10n
 
+p10ns = [
+    p10n.Constant(jnp.pi),
+    p10n.PiecewiseLinear(jnp.linspace(-4, 6, 200)),
+    p10n.Chebyshev(jnp.zeros(32)),
+]
+
 
 class TestParameterization:
 
-    @pytest.mark.parametrize(
-    "parameterization",
-    [
-    p10n.Constant(jnp.pi),
-    ]
-    )
+    @pytest.mark.parametrize("parameterization", p10ns)
     def testFinite(self, parameterization):
+        """Test that p10ns produce real results on [0, 1]"""
         xs = jnp.linspace(0, 1, 100)
         ys = parameterization(xs)
+        print(xs, ys)
         assert np.isfinite(ys).all()
+
+    @pytest.mark.parametrize("parameterization", p10ns)
+    def testFlattenUnflatten(self, parameterization):
+        """Test that p10ns can be flattened and unflattened by JAX."""
+        flattened, structure = jax.tree_flatten(parameterization)
+        unflattened = jax.tree_unflatten(structure, flattened)
+        import pdb; pdb.set_trace()
+        assert parameterization == unflattened
