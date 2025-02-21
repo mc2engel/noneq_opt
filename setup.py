7ea7d470ba2cb49b5d3b1dbe6ea5a8c9ae106e83
mc2engel <50532257+mc2engel@users.noreply.github.com>
2025-02-21 14:42:39 -0700

    Update setup.py
    fixing broken dependencies

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setuptools.setup(
   license='MIT',
   packages=setuptools.find_packages(),
   install_requires=[
-    "jax",
+    "jax==0.4.7",
     "jax-cosmo",
-    "jax-md",
+    "jax-md==0.2.6",
     "matplotlib",
     "numpy",
     "pandas",
